e48ba074e473f6bb673a4a44a8de5d24a4c557a8
Rex Gatchalian <rexg744@gmail.com>
2023-10-21 17:28:30 +0800

    verification update

--- oopmidterms.py
+++ oopmidterms.py
@@ -1,6 +1,6 @@
 class Student:
     
-    def __init__ (self, name, student_number, year_level, program):
+    def __init__ (self, name, year_level, program):
         pass
 
 class Registrar:
@@ -9,19 +9,54 @@ class Registrar:
 class Cashier:
     pass
 
-class Clearance:
-    pass
+class Clearance():
+    def __init__(self, name, year_level, program):
+        self.name = name
+        self._student_number = student_number
+        self.yearlevel = year_level
+        self.program = program
+        self.OrgFee = False
+        self.CsgFee = False
+        self.status = True
+
+    def get(self):
+        return (self._student_number)
+
+    def PayOrg(self):
+        if {self.OrgFee}== True:
+            return f'{self.name} paid the Org Fee.'
+
+    def CSGFee(self):
+        if {self.CsgFee} == True:
+            return f'{self.CsgFee} paid the CSG Fee.'
+
+    def signing(self):
+        if {self.status}==False:
+            return f'The clearance of {self.name} with a student number of {self._student_number}///' \
+                   f' and a {self.year_level} from the program of {self.program} is already signed. '
 
 
-class Encoder(Student):
+class Verification(Student, Cashier):
 
-    Student.__init__(name, student_number, year_level, program)
+    def __init__(self, name, year_level, program, status):
+        self.verify = True
 
-    def checkClearance(self):
-        pass
+        Student.__init__(self, name, program, year_level)
+        Cashier.__init__(self, status)
 
-    def encodeCourses(self):
-        pass
+    def verify(self):
+        if Cashier.status() == True:
+            print(f'{Student.name} is cleared ')
+        else: 
+            print('Please clear your previous requirements')
 
-    def prospectus(self):
-        pass
+    def details(self):
+        if Cashier.status() == True:
+            print(f'{Student.name}of {Student.year_level} - {Student.program} \
+                  has complete all the requirement for enrollment.')
+            print(f'{Student.name} is officially enrolled')
+
+
+
+
+student1 = Clearance("Spledelyn Cristine Recarze",202280045,"2nd Year","BS Computer Science")
