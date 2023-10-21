d203e9da1d9e958d5fbfc0d7b18adc23aaa3fef3
qtchimken <spledrecarze@gmail.com>
2023-10-21 16:59:28 +0700

    add clearance

--- oopmidterms.py
+++ oopmidterms.py
@@ -9,50 +9,20 @@ class Registrar:
 class Cashier:
     pass
 
-class clearance():
-    def __init__(self,Name,StudentNumber,YearLevel,Program):
-        self.name = Name
-        self._studentnumber = StudentNumber
-        self.yearlevel = YearLevel
-        self.program = Program
-        self.OrgFee = False
-        self.CsgFee = False
-        self.status = True
+class Clearance(Student):
+    def __init__(self):
+        self.orgfee = False
+        self.csgfee = False
 
-    def get(self):
-        return (self._studentnumber)
+        Student.__init__(self,name,year_level,program)
 
-    def PayOrg(self):
-        if {self.OrgFee}== True:
-            return f'{self.name} paid the Org Fee.'
-
-    def CSGFee(self):
-        if {self.CsgFee} == True:
-            return f'{self.CsgFee} paid the CSG Fee.'
-
-    def signing(self):
-        if {self.status}==False:
-            return f'The clearance of {self.name} with a student number of {self._studentnumber}///' \
-                   f' and a {self.yearlevel} from the program of {self.program} is already signed. '
-
-student1 = clearance("Spledelyn Cristine Recarze",202280045,"2nd Year","BS Computer Science")
-
-
-print(student1.name)
-print(student1.yearlevel)
-print(student1.program)
-print(student1.OrgFee)
-print(student1.CsgFee)
-print(student1.status)
-
-
-
-
-
-
-
-
-
+    def Pay_Org(self):
+        if self.orgfee==True:
+            print(f'Already cleared in Org Fee.')
+        elif self.csgfee==True:
+            print(f'Already cleared in CSG Fee.')
+        else:
+            print(f'Is not cleared.')
 
 class Encoder(Student):
 
